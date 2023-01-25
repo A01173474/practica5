cedfc26a014846fb49c19bc2af91d578a736eb80
A01173474 <A01173474@tec.mx>
2023-01-24 23:02:29 -0600

    agregar accion de botones

--- js/index.js
+++ js/index.js
@@ -1,10 +1,27 @@
-$ ('.agregar').click(function(){
-    let producto = $("#newText").val()
-    let elemento = $("<li>")
-    elemento.text(producto)
-    $(".Lista").append(elemento)
+$ ('.agregar').click(function(e){  //Boton agregar
+    e.preventDefault();
+    let texto = $("#newText").val();
+
+    if (texto != ""){
+        $(".Lista").append(`
+        <div clas="shopItem">
+        <li class="lis">
+        <p class= "itemShop">${texto}</p>
+        <button class= "checar" type ="text"> Check </button>
+        <button class= "del" type="text"> Delete </button>
+        <p></p>
+        `);
+        
+    }
 })
 
-$(".Lista").on("click", 'li', function(){
-    $(this).toggleClass('done')
-  })
+$(".Lista").on('click','checar',function(event){  //Boton check
+    event.preventDefault();
+    $(this).parent().toggleClass('chec')
+})
+
+
+$(".Lista").on('click','del',function(event){  //Boton borrar
+    event.preventDefault();
+    $(this).parent().parent().remove();
+})
